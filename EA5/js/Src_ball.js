ec2b6a83efbbf471bc2476e41f4795c9699bbfb0
Laura2702 <l.pichlmeier@gmx.de>
2022-11-20 12:00:36 +0100

    Add files via upload

---
+++ EA5/js/Src_ball.js
@@ -0,0 +1,84 @@
+var ball = ( function() {
+
+
+	function createVertexData() {
+		var n = 32;
+		var m = 32;
+
+		// Positions.
+		this.vertices = new Float32Array(3 * (n + 1) * (m + 1));
+		var vertices = this.vertices;
+		// Normals.
+		this.normals = new Float32Array(3 * (n + 1) * (m + 1));
+		var normals = this.normals;
+		// Index data.
+		this.indicesLines = new Uint16Array(2 * 2 * n * m);
+		var indicesLines = this.indicesLines;
+		this.indicesTris = new Uint16Array(3 * 2 * n * m);
+		var indicesTris = this.indicesTris;
+
+		var du = 2 * Math.PI/n;
+    var dv = Math.PI/m;
+		var r = 0.72;
+		// Counter for entries in index array.
+		var iLines = 0;
+		var iTris = 0;
+
+		// Loop u.
+        for(var i = 0, u = 0; i <= n; i++, u += du) {
+            // Loop v.
+            for(var j = 0, v = 0; j <= m; j++, v += dv) {
+
+                var iVertex = i * (m + 1) + j;
+
+								var x = r*Math.sin(v)*Math.cos(u) + 1.5;
+                var z = r*Math.sin(v)*Math.sin(u) + 1;
+                var y = r*Math.cos(v);
+
+                // Set vertex positions.
+                vertices[iVertex * 3] = x;
+                vertices[iVertex * 3 + 1] = y;
+                vertices[iVertex * 3 + 2] = z;
+
+                // Calc and set normals.
+								var nx = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+								normals[iVertex * 3] = x / nx;
+								normals[iVertex * 3 + 1] = y / nx;
+								normals[iVertex * 3 + 2] = z / nx;
+
+				// if(i>14){
+				// continue;
+				// }
+
+				// Set index.
+				// Line on beam.
+				if(j > 0 && i > 0) {
+					indicesLines[iLines++] = iVertex - 1;
+					indicesLines[iLines++] = iVertex;
+				}
+				// Line on ring.
+				if(j > 0 && i > 0) {
+					indicesLines[iLines++] = iVertex - (m + 1);
+					indicesLines[iLines++] = iVertex;
+				}
+
+				// Set index.
+				// Two Triangles.
+				if(j > 0 && i > 0) {
+					indicesTris[iTris++] = iVertex;
+					indicesTris[iTris++] = iVertex - 1;
+					indicesTris[iTris++] = iVertex - (m + 1);
+					//
+					indicesTris[iTris++] = iVertex - 1;
+					indicesTris[iTris++] = iVertex - (m + 1) - 1;
+					indicesTris[iTris++] = iVertex - (m + 1);
+				}
+			}
+		}
+	}
+
+	return {
+		createVertexData : createVertexData
+	}
+
+}());
